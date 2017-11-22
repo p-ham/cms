5fcf09269e4b4b042cf4ddbc830eaa7766d305c6
root <root@Neptun.localdomain>
2017-11-22 14:00:41 +0100

    add responsive nav, update paragraph style, fix responsive font-size

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,1 +1,6 @@
-console.log("I go first!");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+    nav.classList.toggle("open");
+});
